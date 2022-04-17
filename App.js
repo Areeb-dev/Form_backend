85270e91b9c02ad23ee26543a8a25495e1754149
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2022-04-17 10:45:37 +0500

    add .env file and also update port

--- App.js
+++ App.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/Signup");
-
+require('dotenv').config()
 const signupRoute = require("./routes/signUpRoute");
-
+const PORT = process.env.PORT || 4000
 app.use(express.json());
 
 app.use("/", signupRoute);
 
-app.listen(3001, () => {
-  console.log("Port is working");
+app.use('/get', (req, res) => {
+  try {
+    const data = "hello this is example"
+    return res.send({ success: true, message: data })
+  } catch (error) {
+    return res.send({ success: false, message: 'something went worng' })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log("Server is working");
 });
